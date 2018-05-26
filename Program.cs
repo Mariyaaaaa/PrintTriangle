89b9d49a011d74f62e99a55cfb5578742a5b47e7
Mariya <mariqvalkova@gmail.com>
2018-05-26 20:09:13 +0300

    PrintTriangle
    Triangle from given number

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintTriangle(number);
+        }
+
+        static void PrintTriangle(int n)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                PrintLine(1, j);
+            }
+
+            for (int k = n - 1; k >= 1; k--)
+            {
+                PrintLine(1, k);
+            }
+
+
+        }
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
